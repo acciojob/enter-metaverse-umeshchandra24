12c392fe5bdaad3c10fe62b9788bcf4e293ffac3
umeshchandra24 <120272310+umeshchandra24@users.noreply.github.com>
2023-05-19 06:58:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function myFunction(){
+    var para = document.getElementById("status");
+    var newH1 = document.createElement("h1");
+    newH1.textContent="Entered Metaverse";
+    para.parentNode.replaceChild(newH1, para);
+}
